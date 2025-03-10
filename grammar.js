8c33ecafb7459b46e64b5d39818d78de1b121ffb
Jonas Hoersch <jonas.hoersch@posteo.de>
2025-03-10 16:51:23 +0100

    Remove indirection for different separators and separate type layer

--- grammar.js
+++ grammar.js
@@ -1,74 +1,43 @@
-let rules_for_sep = (sep, sym) => {
-  let r = {};
-  r[`_csv${sym}`] = ($) => repeat1(alias($[`row${sym}`], $.row));
-  r[`row${sym}`] = ($) =>
-    seq(
-      repeat(seq($[`_cycle7${sym}`], sep)),
-      optional($[`_remainder${sym}`]),
-      "\n",
-    );
-
-  r[`_cycle7${sym}`] = ($) =>
-    seq(
-      optional(alias($[`field${sym}`], $.first)),
-      sep,
-      optional(alias($[`field${sym}`], $.second)),
-      sep,
-      optional(alias($[`field${sym}`], $.third)),
-      sep,
-      optional(alias($[`field${sym}`], $.fourth)),
-      sep,
-      optional(alias($[`field${sym}`], $.fifth)),
-      sep,
-      optional(alias($[`field${sym}`], $.sixth)),
-      sep,
-      optional(alias($[`field${sym}`], $.seventh)),
-    );
-  r[`_remainder${sym}`] = ($) =>
-    seq(
-      alias($[`field${sym}`], $.first),
-      ...[$.seventh, $.sixth, $.fifth, $.fourth, $.third, $.second].reduce(
-        (accum, fld) => [
-          optional(seq(sep, optional(alias($[`field${sym}`], fld)), ...accum)),
-        ],
-        [],
-      ),
-    );
-
-  r[`field${sym}`] = ($) =>
-    choice(
-      prec(3, $.boolean),
-      prec(2, $.number),
-      prec(1, $.float),
-      prec(0, alias($[`text${sym}`], $.text)),
-    );
-  r[`text${sym}`] = (_) =>
-    token(
-      choice(
-        new RegExp(`[^-+${sep}\\d\\s"][^${sep}\\n\\r"]+`),
-        seq('"', repeat(choice(/[^"]/, '""')), '"'),
-      ),
-    );
-  return r;
-};
-
+let sep = ",";
 module.exports = grammar({
   name: "csv",
 
   rules: {
-    csv: ($) => choice($._csvc, $._csvs, $._csvp),
+    csv: ($) => seq(repeat(seq($.row, "\n")), optional($.row)),
+    row: ($) => seq(repeat(seq($.cycle7, sep)), $.remainder),
 
-    ...rules_for_sep(",", "c"),
-    ...rules_for_sep(";", "s"),
-    ...rules_for_sep("|", "p"),
+    cycle7: ($) =>
+      seq(
+        optional(alias($.field, $.first)),
+        sep,
+        optional(alias($.field, $.second)),
+        sep,
+        optional(alias($.field, $.third)),
+        sep,
+        optional(alias($.field, $.fourth)),
+        sep,
+        optional(alias($.field, $.fifth)),
+        sep,
+        optional(alias($.field, $.sixth)),
+        sep,
+        optional(alias($.field, $.seventh)),
+      ),
 
-    number: (_) => choice(/[-+]?\d+/, /0[xX][0-9a-fA-F]+/),
-    float: (_) => choice(/\d*\.\d+/, /[-+]?\d+\.\d*/),
-    boolean: (_) => choice("true", "false"),
+    remainder: ($) =>
+      seq(
+        alias($.field, $.first),
+        ...[$.seventh, $.sixth, $.fifth, $.fourth, $.third, $.second].reduce(
+          (accum, fld) => [
+            optional(seq(sep, optional(alias($.field, fld)), ...accum)),
+          ],
+          [],
+        ),
+      ),
+
+    field: ($) =>
+      choice(
+        new RegExp(`[^${sep}\\n\\r\"]+`),
+        seq('"', repeat(choice(/[^"]/, '""')), '"'),
+      ),
   },
-
-  conflicts: ($) => [
-    [$.rowc, $.rows, $.rowp],
-    [$.fieldc, $.fields, $.fieldp],
-  ],
 });
